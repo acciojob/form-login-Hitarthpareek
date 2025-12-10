dcef0e259c7e1eaf64b6c2c1ff000653fb504ee9
Hitarthpareek <80564705+Hitarthpareek@users.noreply.github.com>
2025-12-10 14:42:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+    const form = document.getElementById("form1");
+    const fname = form.elements["fname"].value.trim();
+    const lname = form.elements["lname"].value.trim();
 
+	const filterdFirstName = fname.
+	if(fname.length===0 || lname.length===0) return
+	
+
+	  alert(fname + " " + lname);
 }
